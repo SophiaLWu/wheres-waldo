05f1238c5392403cefa4d4f8aec1dac40464e83b
Sophia Wu <sophia.luxi.wu@gmail.com>
2017-04-27 23:26:41 -0700

    Add functionality of checking if user found the correct character chosen

--- app/assets/javascripts/puzzles.js
+++ app/assets/javascripts/puzzles.js
@@ -1,38 +1,67 @@
 $(document).ready(function() {
 
+  var mouseX;
+  var mouseY;
+  var target_box_width = parseInt($(".target-box").css("width"), 10);
+  var target_box_height = parseInt($(".target-box").css("height"), 10);
+
   var hideTargetInfo = function() {
-    $(".target-choices").css("visibility", "hidden")
-    $(".target-box").css("visibility", "hidden")
+    $(".target-choices").css("visibility", "hidden");
+    $(".target-box").css("visibility", "hidden");
   };
 
-  var findTarget = function() {
+  var photoOnClick = function() {
     $("#puzzle-image").on("click", function(e) {
       if ($(".target-box").css("visibility") === "visible") {
         hideTargetInfo();
       }
       else {
-        var mouseX = e.pageX;
-        var mouseY = e.pageY;
-        var target_box_width = parseInt($(".target-box").css("width"), 10);
-        var target_box_height = parseInt($(".target-box").css("height"), 10);
+        mouseX = e.pageX - $('#puzzle-image').offset().left;
+        mouseY = e.pageY - $('#puzzle-image').offset().top;
         $(".target-choices").css({ visibility: "visible",
-                                          top: mouseY - target_box_height/2 + "px",
-                                          left: mouseX + target_box_width/2 + "px" });
+                                          top: mouseY - target_box_height/2 
+                                               + "px",
+                                          left: mouseX + target_box_width/2 
+                                                + "px" });
         $(".target-box").css({ visibility: "visible",
                                       top: mouseY - target_box_height/2 + "px",
-                                      left: mouseX - target_box_width/2 + "px" });
+                                     left: mouseX - target_box_width/2
+                                           + "px" });
       }
     });
+  };
 
+  var targetInfoOnClick = function() {
     $(".target-box").on("click", function(e) {
-    hideTargetInfo();
+      hideTargetInfo();
     });
 
     $(".target-choices").on("click", function(e) {
       hideTargetInfo();
     });
+  };
+
+  var getCharacterPosition = function($character) {
+    var x_position = parseInt($("meta[name='" + $character.data("name") +
+                                "']").attr("x_position"), 10);
+    var y_position = parseInt($("meta[name='" + $character.data("name") +
+                                "']").attr("y_position"), 10);
+    return [x_position, y_position];
+  };
+
+  var checkGuess = function() {
+    $(".character").on("click", function() {
+      var characterPosition = getCharacterPosition($(this));
+      if (mouseX - target_box_width/2 <= characterPosition[0] &&
+          characterPosition[0] <= mouseX + target_box_width/2 &&
+          mouseY - target_box_height/2 <= characterPosition[1] &&
+          characterPosition[1] <= mouseY + target_box_height/2) {
+        alert("FOUND");
+      }
+    });
   }
 
-  findTarget();
-  
+  photoOnClick();
+  targetInfoOnClick();
+  checkGuess();
 });
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170428004951) do
+ActiveRecord::Schema.define(version: 20170428041556) do
 
   create_table "characters", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170428004951) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "puzzle_id"
+    t.string   "image_url"
   end
 
   create_table "puzzles", force: :cascade do |t|
