31b1b469e7560b8fe5bc14a87b0ae38983ca8866
Sophia Wu <sophia.luxi.wu@gmail.com>
2017-04-27 23:04:22 -0700

    Add seeds for Gold Rush puzzle

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puzzles = Puzzle.create([{ name: "The Gold Rush", 
+                           image_url: "gold-rush.jpg" }])
+
+characters = Character.create([{ name: "Waldo",
+                                 x_position: 250,
+                                 y_position: 385,
+                                 puzzle_id: 1,
+                                 image_url: "waldo.jpg" },
+                               { name: "Wizard Whitebeard",
+                                 x_position: 1307,
+                                 y_position: 226,
+                                 puzzle_id: 1,
+                                 image_url: "whitebeard.jpg" },
+                               { name: "Odlaw",
+                                 x_position: 524,
+                                 y_position: 99,
+                                 puzzle_id: 1,
+                                 image_url: "odlaw.jpg" },
+                               { name: "Wenda",
+                                 x_position: 811,
+                                 y_position: 720,
+                                 puzzle_id: 1,
+                                 image_url: "wenda.jpg" },
+                               { name: "Woof",
+                                 x_position: 1014,
+                                 y_position: 184,
+                                 puzzle_id: 1,
+                                 image_url: "woof.jpg" }])
